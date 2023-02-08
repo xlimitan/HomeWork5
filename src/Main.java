2434bdd7eea1f75a8c579e8cf77fbbe0e0a826a9
xlimitan <xlimitan@yandex.ru>
2023-02-08 13:54:31 +0300

    Задача № 2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
         int clientOs = 0;
         if (clientOs == 0) {
@@ -14,7 +15,22 @@ public class Main {
         }
     }
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Задача 2");
+        int clientOs = 1;
+        int clientDeviceYear = 2010;
+        if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
     }
 }
