a1b74fdb95018ffd33baff6380264f02769b69c2
xlimitan <xlimitan@yandex.ru>
2023-02-08 15:35:25 +0300

    Задача № 5

--- src/Main.java
+++ src/Main.java
@@ -60,5 +60,32 @@ public class Main {
     }
     public static void task5() {
         System.out.println("Задача 5");
+
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого времени года нету");
+        }
     }
 }
