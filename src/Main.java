341745fcc76c0ca7d90f841f914297993c2aa554
xlimitan <xlimitan@yandex.ru>
2023-02-08 13:46:10 +0300

    Задача № 1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        int clientOs = 0;
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 
     public static void task2 () {
